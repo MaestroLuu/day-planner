f47e62a76d1b0eecd49854778e58768dac144ac9
David Luu <dluu.audeo@gmail.com>
2022-01-07 00:59:56 -0800

    created if statement for localStorage display

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,59 +1,42 @@
-var time = moment("1:23PM", "h:mmA");
+var time = moment();
 var dateDisplay = $('#currentDay');
 var saveBtn = $(".saveBtn");
-var hour9 = $("#hour-9");
-var hour10 = $("#hour-10");
-var hour11 = $("#hour-11");
-var hour12 = $("#hour-12");
-var hour13 = $("#hour-13");
-var hour14 = $("#hour-14");
-var hour15 = $("#hour-15");
-var hour16 = $("#hour-16");
-var hour17 = $("#hour-17");
-var hourArray = [hour9, hour10, hour11, hour12, hour13, hour14, hour15, hour16, hour17];
 var currentTime = time.format("k");
+
+displayDate();
 
 $(".time-block").each(function() {
     var hour = $(this).attr("id").split("-");
     var newId = $(this).attr("id", hour[1]);
+    var newValue = parseInt($(this).attr("id"));
 
-    if (parseInt($(this).attr("id")) == currentTime) {
+    if (newValue == currentTime) {
          $(this).addClass("present");
+    } else if (newValue < currentTime) {
+        $(this).addClass("past");
+    } else {
+        $(this).addClass("future");
+    }
+
+    if (localStorage.getItem(newValue)) {
+        $(this).class("description").text(localStorage.getItem(newValue));
     }
     
-    console.log(parseInt($(this).attr("id")));
 
-    // 
-    //all logic inside here using $(this)
+    // if (newValue == localStorage.key(newValue)) {
+    //     $(this).children.eq(1).text(localStorage.getItem(newValue));
+    // }
+
+
+    // if newValue = key from localStorage
+    // pull value and add the text to corresponding id
+
+    // if (localStorage.key == newValue) {
+    //     // $(this).text(localStorage.getItem(newValue));
+        
+    // }
 });
 
-// checking value type
-
-
-// function test() {
-//     for (i = 0; i < hourArray.length; i++) {
-        
-//         // remember to change out hourArray[4].text to testTime
-//         if (hourArray[i].children().eq(0).text() === currentTime) {
-//             hourArray[i].addClass("present");
-//         } else if (hourArray[i] < hourArray[4].text()) {
-//             hourArray[i].addClass("past");
-//         } else {
-//             hourArray[i].addClass("future");
-//         }
-//     }
-// }
-
-// test();
-displayDate();
-// displayTasks();
-
-// just try one first
-// assign a value to each hour
-// designated assignedHour from moments
-// if presentTime = assignedHour
-// present class is appended
-// ELSE if presentTime > than assignedHour
 
 saveBtn.on("click", function(event) {
     var hour = $(this).parent().attr("id");
