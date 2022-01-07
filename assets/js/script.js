a5a7d5c5b02f631a78464067b3cfa2c747d96037
David Luu <dluu.audeo@gmail.com>
2022-01-06 23:22:31 -0800

    replaced hard coded array with class inspect loop; recoded if statements

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,6 @@
-var time = moment();
+var time = moment("1:23PM", "h:mmA");
 var dateDisplay = $('#currentDay');
 var saveBtn = $(".saveBtn");
-var hour = time.format("k");
-
-
 var hour9 = $("#hour-9");
 var hour10 = $("#hour-10");
 var hour11 = $("#hour-11");
@@ -13,37 +10,41 @@ var hour14 = $("#hour-14");
 var hour15 = $("#hour-15");
 var hour16 = $("#hour-16");
 var hour17 = $("#hour-17");
+var hourArray = [hour9, hour10, hour11, hour12, hour13, hour14, hour15, hour16, hour17];
+var currentTime = time.format("k");
 
-var hourArray = [hour9, hour10, hour11, hour12, hour13, hour14, hour15, hour16, hour17]
-// checking value type
-var currentTime = moment("1:23PM", "H:mmA").format("hA");
-console.log(currentTime);
+$(".time-block").each(function() {
+    var hour = $(this).attr("id").split("-");
+    var newId = $(this).attr("id", hour[1]);
 
-var mushu = hourArray[4].children().eq(0).text();
-console.log(mushu);
-
-var bigMac = JSON.stringify(hourArray[4].children().eq(0).text());
-console.log(bigMac);
-
-var whopper = JSON.stringify(currentTime);
-console.log(whopper);
-
-function test() {
-    for (i = 0; i < hourArray.length; i++) {
-        
-        // remember to change out hourArray[4].text to testTime
-        if (hourArray[i].text() === currentTime) {
-            console.log(hourArray[4].text() === currentTime);
-            hourArray[i].addClass("present");
-        // } else if (hourArray[i] < hourArray[4].text()) {
-        //     hourArray[i].addClass("past");
-        // } else {
-        //     hourArray[i].addClass("future");
-        }
+    if (parseInt($(this).attr("id")) == currentTime) {
+         $(this).addClass("present");
     }
-}
+    
+    console.log(parseInt($(this).attr("id")));
 
-test();
+    // 
+    //all logic inside here using $(this)
+});
+
+// checking value type
+
+
+// function test() {
+//     for (i = 0; i < hourArray.length; i++) {
+        
+//         // remember to change out hourArray[4].text to testTime
+//         if (hourArray[i].children().eq(0).text() === currentTime) {
+//             hourArray[i].addClass("present");
+//         } else if (hourArray[i] < hourArray[4].text()) {
+//             hourArray[i].addClass("past");
+//         } else {
+//             hourArray[i].addClass("future");
+//         }
+//     }
+// }
+
+// test();
 displayDate();
 // displayTasks();
 
